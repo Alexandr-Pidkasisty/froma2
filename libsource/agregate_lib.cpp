9b6ae5ee419372eea37c487dfce751a50897f054
Alexandr Pidkasisty <app.business@mail.ru>
2026-07-14 21:30:10 +0300

    Скорректировал комментарии в модуле Agregate

--- libsource/agregate_lib.cpp
+++ libsource/agregate_lib.cpp
@@ -317,7 +317,7 @@ void clsAgregate::Set_autopurchase(const size_t index, const PurchaseCalc _pcalc
 
 bool clsAgregate::Set_ship_volume(const strItem _Shipment[]) {
 /** Метод ввода объемов отгрузки в подразделение с самым большим индексом. Параметры: _Shipment
-- указатель на вводимый массив с отгрузками. Метод возвращает True при удачном копировании входного
+- указатель на вводимый массив с отгрузками. Метод возвращает true при удачном копировании входного
 массива во внутренний массив Shipment.**/
     if((!_Shipment)  || (CostChain.size() == sZero)) return false;  // Валидация параметров
     sh_size = std::visit(Getter_exporting_count(shipment), *(CostChain.cend()-1));  // Получаем число строк массива
@@ -332,7 +332,7 @@ bool clsAgregate::Set_ship_volume(const strItem _Shipment[]) {
 
 bool clsAgregate::Set_pur_price(const strItem _Purchase[]) {
 /** Метод ввода цен поставок в подразделение с нулевым индексом. Параметры: _Purchase - указатель на вводимый
-массив с поставками. Метод возвращает True при удачном копировании входного массива во внутренний массив Purchase. **/
+массив с поставками. Метод возвращает true при удачном копировании входного массива во внутренний массив Purchase. **/
     if((!_Purchase)  || (CostChain.size() == sZero)) return false;  // Валидация параметров
     sh_size = std::visit(Getter_exporting_count(purchase), *(CostChain.cbegin()));  // Получаем число строк массива
     size_t tcount = sh_size * PrCount;              // Вычисляем размер копируемых массивов
@@ -356,7 +356,8 @@ bool clsAgregate::Set_pur_price(const strItem _Purchase[]) {
 bool clsAgregate::Set_pur_volume(const strItem _Pur_vol[]) {
 /** Метод ввода объемов поставок в подразделение типа clsStorage с самым низким индексом. Используется в случае, когда
 хотя бы для одного SKU установлен флаг ручного ввода объемов поставок (флаг "nocalc" - без автоматического расчета).
-ВНИМАНИЕ!!! Метод вызывается ДО вызова функции RCalculate, примерно рядом с вызовом метода Set_ship_volume. **/
+ВНИМАНИЕ!!! Метод вызывается ДО вызова функции RCalculate, примерно рядом с вызовом метода Set_ship_volume.
+Параметры: _Pur_vol - указатель на вводимый массив с поставками. **/
     if(!_Pur_vol) return false;                             // Валидация параметров
     for(vector<T_agregate>::iterator it = CostChain.begin(); it != CostChain.end(); it++)
         if(std::holds_alternative<clsStorage>(*it)) {       // Если it указывает на clsStorage, то
